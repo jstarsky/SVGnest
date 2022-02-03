6d862f8f2c47bbe703d0ab4b078ba88b10e55a02
Jose  Starsky - Labelgrup <jstarsky@labelgrup.com>
2022-02-03 19:26:20 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 
 const deps = require("./package.json").dependencies;
 module.exports = {
@@ -51,6 +52,21 @@ module.exports = {
     }),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-    }),
+    }),    
+    new AddAssetHtmlPlugin([
+      { filepath: require.resolve('./src/util/clipper') },
+      { filepath: require.resolve('./src/util/domparser') },
+      { filepath: require.resolve('./src/util/eval') },
+      { filepath: require.resolve('./src/util/filesaver') },
+      { filepath: require.resolve('./src/util/geometryutil') },
+      { filepath: require.resolve('./src/util/json') },
+      { filepath: require.resolve('./src/util/matrix') },
+      { filepath: require.resolve('./src/util/parallel') },
+      { filepath: require.resolve('./src/util/pathsegpolyfill') },
+      { filepath: require.resolve('./src/util/placementworker') },
+      { filepath: require.resolve('./src/svgnest') },
+      { filepath: require.resolve('./src/svgparser') },
+
+    ]),
   ],
 };
